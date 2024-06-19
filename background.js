6b8b73366798d57df4f90b12bccc150c2a2c4825
Anurag Mishra <115710545+17AnuragMishra@users.noreply.github.com>
2024-06-19 09:16:05 +0530

    Update background.js

--- background.js
+++ background.js
@@ -1,13 +1,31 @@
-chrome.runtime.onStartup.addListener(function () {
-    pendingTabs.forEach(tab => {
-        if (tab.reminder.type !== 'none') {
-            const notificationOptions = {
-                type: 'basic',
-                iconUrl: 'icon.png',
-                title: `Reminder: ${tab.title}`,
-                message: `This tab has a ${tab.reminder.type} reminder.`
-            };
-            chrome.notifications.create('', notificationOptions);
+chrome.runtime.onInstalled.addListener(() => {
+    chrome.alarms.create('checkReminders', { periodInMinutes: 1 });
+  });
+  
+  chrome.alarms.onAlarm.addListener((alarm) => {
+    if (alarm.name === 'checkReminders') {
+      checkReminders();
+    }
+  });
+  
+  function checkReminders() {
+    chrome.storage.local.get('pendingTabs', (result) => {
+      const now = new Date().getTime();
+      const pendingTabs = result.pendingTabs || [];
+  
+      pendingTabs.forEach((tab, index) => {
+        if (tab.reminderTime && now >= tab.reminderTime) {
+          chrome.notifications.create({
+            type: 'basic',
+            iconUrl: 'icon.png',
+            title: 'Tab Reminder',
+            message: `Time to check this tab: ${tab.title}`
+          });
+  
+          // Remove the reminder after notification
+          delete pendingTabs[index].reminderTime;
+          chrome.storage.local.set({ pendingTabs: pendingTabs });
         }
+      });
     });
-});
+  }
